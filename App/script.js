ac1815880179a8f26f27e9e3177e21dbe9de6c87
oldgreek87-bit <oldgreek87@gmail.com>
2025-12-07 01:04:12 +0700

    Add files via upload

---
+++ App/script.js
@@ -0,0 +1,16 @@
+console.log("Bookflix MiniApp loaded");
+
+Telegram.WebApp.ready();
+Telegram.WebApp.expand();
+
+function openSubscription() {
+  alert("Subscription screen placeholder");
+}
+
+function mySubscription() {
+  alert("Subscription status placeholder");
+}
+
+function readingRoom() {
+  alert("Reading Room placeholder");
+}
